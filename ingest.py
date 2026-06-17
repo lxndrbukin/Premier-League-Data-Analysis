b641bac7df11e0c7694f767288a9eb95c0b5d27b
lxndrbukin <lxndrbukin@proton.me>
2026-06-17 20:46:53 +0100

    Complete the ingest script

---
+++ ingest.py
@@ -0,0 +1,30 @@
+import requests
+import os
+from dotenv import load_dotenv
+import pandas as pd
+from datetime import datetime
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+def main():
+    req = requests.get(
+        'https://api.football-data.org/v4/competitions/2021/standings',
+        headers={'X-Auth-Token': API_KEY}
+    )
+    response = req.json()
+    table = response['standings'][0]['table']
+
+    df_normalized = pd.json_normalize(table)
+
+    df_normalized.columns = [col.replace('team.', '') for col in df_normalized.columns]
+
+    dt = datetime.today().strftime('%Y-%m-%d')
+
+    os.makedirs(f'data/raw/{dt}', exist_ok=True)
+
+    df_normalized.to_csv(f'data/raw/{dt}/standings.csv', index=False)
+
+if __name__ == '__main__':
+    main()
